af803cad6c732d9638dee00659409cbbdf7a981e
dannsch <horrorantonio346@gmail.com>
2024-11-07 17:15:57 -0400

    Code
    Code do Trabalho de POO1

--- src/dominio/Curso.java
+++ src/dominio/Curso.java
@@ -1,63 +1,48 @@
 package dominio;
 
-public class Curso
-{
+public class Curso {
 
     private int codigo;
     private String nome;
     private String descricao;
     private int cargahr;
 
-    // Código do Curso
-
-    public int getCodigo()
-    {
+    public int getCodigo() {
         return codigo;
     }
-    public void setCodigo(int codigo)
-    {
+
+    public void setCodigo(int codigo) {
         this.codigo = codigo;
     }
 
-    // Nome do Curso
-
-    public String getNome()
-    {
+    public String getNome() {
         return nome;
     }
-    public void setNome(String nome)
-    {
+
+    public void setNome(String nome) {
         this.nome = nome;
     }
 
-    // Descrição do Curso
-
-    public String getDescricao()
-    {
+    public String getDescricao() {
         return descricao;
     }
-    public void setDescricao(String descricao)
-    {
+
+    public void setDescricao(String descricao) {
         this.descricao = descricao;
     }
 
-    // Carga Horária do Curso
-
-    public int getCargahr()
-    {
+    public int getCargahr() {
         return cargahr;
     }
-    public void setCargahr(int cargahr)
-    {
+
+    public void setCargahr(int cargahr) {
         this.cargahr = cargahr;
     }
 
-
-
-    public Curso()
-    {
+    public Curso() {
 
     }
+
     public Curso(int codigo, String nome, String descricao, int cargahr) {
         this.codigo = codigo;
         this.nome = nome;
